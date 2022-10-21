c447bcd7bd0e1ca596e7630a65c1671db24e111d
Jinnux <nguyentandat26032001@gmail.com>
2022-10-21 22:51:40 +0700

    done

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# User Manual
+
+- Create .env file in root directory. Define MONGODB_URL = <Your MONGODB connection url> and PORT = <Your port>
+
 # Naming Convention
 
 - Use kebab-case for folder and file name. VD: new-folder, user-auth.controller.js
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
+require('./src/database/conn');
 const expressFileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
 const cors = require('cors');
 
 HOST = 'http://localhost';
@@ -19,25 +19,14 @@ app.use(
   }),
 );
 
-const DB_URL = process.env.MONGODB_URL;
-const db = mongoose.connection;
-//connect db
-mongoose
-  .connect(DB_URL, { useNewUrlParser: true })
-  .then(() => console.log('DB Connected!'));
-db.on('error', (err) => {
-  console.log('DB connection error:', err.message);
-});
-
 app.get('/', function (req, res) {
   res.send(`Server is running at ${HOST}:${PORT}`);
 });
-
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-});
-
 app.use('/user', require('./src/routers/users.route'));
 app.use('/api', require('./src/routers/category.route'));
 app.use('/api', require('./src/routers/products.route'));
 app.use('/api', require('./src/routers/payments.route'));
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
