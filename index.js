938b958f4db083935d677ee26f9de38f42e9f216
Jinnux <nguyentandat26032001@gmail.com>
2022-10-21 21:57:18 +0700

    temp

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
-HOST = "http://localhost";
-PORT = 3000;
+require('dotenv').config();
+const expressFileUpload = require('express-fileupload');
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
 
-const bodyParser = require("body-parser");
-const express = require("express");
+HOST = 'http://localhost';
+PORT = process.env.PORT || 3000;
+
 app = express();
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// parse application/json
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  expressFileUpload({
+    useTempFiles: true,
+  }),
+);
 
-app.get("/", function (req, res) {
+const URI = process.env.MONGODB_URL;
+
+app.get('/', function (req, res) {
   res.send(`Server is running at ${HOST}:${PORT}`);
 });
 
 app.listen(PORT, () => {
-  console.log("Server is listening...");
+  console.log(`App listening on port ${PORT}`);
 });
+
+// app.use('/user', require('./routers/users.route'));
+// app.use('/api', require('./routers/category.route'));
+// app.use('/api', require('./routers/upload'));
+// app.use('/api', require('./routers/products.route'));
+// app.use('/api', require('./routers/payments.route'));
