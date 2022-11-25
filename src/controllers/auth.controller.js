814fb1a6422d1fb59ba62f8ed6caed6c767ec073
hoangthien <thien.dohoangcs98@gmail.com>
2022-11-25 20:34:40 +0700

    update login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -39,7 +39,47 @@ class AuthController {
     });
   }
   async login(req, res) {
-    res.send('Logined');
+    try {
+      const { email, password } = req.body;
+      var user = await Users.findOne({
+        email,
+      });
+
+      if (!user)
+        return res
+          .status(500)
+          .json({ err: err.messages, error: 'User has not been registered' });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch)
+        return res.status(400).json({ msg: 'Authentification failed' });
+
+      const accessToken = createAccessToken({
+        email: user.email,
+        password: password,
+      });
+      const refreshToken = createRefreshToken({
+        email: user.email,
+        password: password,
+      });
+      // const accessToken = await createAccessToken({
+      //   email: 'tandat2603',
+      //   password: 'password',
+      // });
+      // const refreshToken = createRefreshToken({
+      //   email: 'tandat2603',
+      //   password: 'password',
+      // });
+
+      res.cookie('refreshtoken', refreshToken, {
+        path: '/user/refresh_token',
+        httpOnly: true,
+      });
+
+      res.json({ accessToken });
+    } catch (err) {
+      return res.status(500).json({ message: err });
+    }
   }
 }
 function createAccessToken(user) {
