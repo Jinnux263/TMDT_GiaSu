bafbf8129a1b1e6cea1aa232b2f562d8778201df
hoangthien <thien.dohoangcs98@gmail.com>
2022-12-06 23:23:28 +0700

    add cors in src/index.js

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const expressFileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const cors = require('cors');
 HOST = 'http://localhost';
 PORT = process.env.PORT || 3000;
 
 app = express();
+app.use(cors({ origin: true, credentials: true }));
 app.use(bodyParser.json());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -24,12 +25,12 @@ app.get('/', function (req, res) {
 });
 app.use('/grade', require('./src/routers/grade.route'));
 app.use('/subject', require('./src/routers/subject.route'));
-app.use('/', require('./src/routers/auth.route'));
 app.use('/user', require('./src/routers/user.route'));
 app.use('/customer', require('./src/routers/customer.route'));
 app.use('/course', require('./src/routers/course.route'));
 app.use('/tutor', require('./src/routers/tutor.route'));
 app.use('/transaction', require('./src/routers/transaction.route'));
+app.use('/', require('./src/routers/auth.route'));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
